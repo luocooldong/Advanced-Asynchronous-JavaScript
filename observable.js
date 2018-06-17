6d16e8dccb67321a42264d8e8826105d5b547026
gengyudong <gengyd@ivymobi.com>
2018-06-17 20:18:11 +0800

    MINIMAL OBSERVABLE IMPLEMETATION

--- observable.js
+++ observable.js
@@ -25,7 +25,6 @@ class Observable {
     return new Observable(function subscribe(observer){
       for(let num; true; num++){
         observer.next(num);
-
       }
     })
   }
@@ -34,11 +33,9 @@ class Observable {
   static fromEvent(dom, eventName) {
     return new Observable(function subscribe(observer){
       const handler = ev => {
-        let observer = null;
         observer.next(ev);
       }
       dom.addEventListener(eventName, handler);
-
       return {
         unsubscribe() {
           dom.removeEventListener(eventName, handler);
@@ -46,15 +43,85 @@ class Observable {
       }
     });
   }
+
+  map(projection) {
+    const self = this;
+    return new Observable(function subscribe(observer){
+      const subscription = self.subscribe({
+        next(v){
+          let value;
+          try{
+            value = projection(v);
+            observer.next(projection(v));
+          }catch(e){
+            observer.error(e);
+          }
+        },
+        error(err){
+          observer.error(err);
+          subscription.unsubscribe();
+        },
+        complete(){
+          observer.complete();
+        }
+      });
+
+      return subscription;
+    })
+  }
+
+  filter(predicate) {
+    const self = this;
+    return new Observable(function subscribe(observer){
+      const subscription = self.subscribe({
+        next(v){
+          if(predicate(v)){
+            observer.next(v);
+          }
+        },
+        error(err){
+          observer.error(err);
+          subscription.unsubscribe();
+        },
+        complete(){
+          observer.complete();
+        }
+      });
+
+      return subscription;
+    })
+  }
+
+
 }
 
+const button = document.getElementById("button");
 
-const obs = Observable.timeout(500);
-obs.subscribe({
+const clicks = Observable.fromEvent(button, "click");
+
+
+clicks
+.map(ev => ev.offsetX)
+.filter(offsetX => offsetX > 10)
+.subscribe({
   next(v) {
-    console.log('next');
+    console.log('next', v);
   },
   complete() {
     console.log('done');
   }
-})
+});
+
+
+
+// const obs = Observable.timeout(500);
+
+// obs.map(x => x + 1);
+// obs.subscribe({
+//   next(v) {
+//     console.log('next');
+//   },
+//   complete() {
+//     console.log('done');
+//   }
+// })
